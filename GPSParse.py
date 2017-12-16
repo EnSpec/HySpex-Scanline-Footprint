553c417c14f3563e53e123b8118344105b8b7f4c
Matthew Westphall <westphall@russell.wisc.edu>
2017-12-16 16:57:46 -0600

    fixed issues with sign on roll and pitch and added README

--- GPSParse.py
+++ GPSParse.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python3
 import numpy as np
 import LonLatMath as llmath
 import requests
 import SHPParse
 
-KEY = open(".secret/key.txt",'r').read().strip()
+KEY = open("key.txt",'r').read().strip()
 
 def getElevation(coord):
-    #don't want to use API too much right off the bat
-    return 186
+    print("Using Google API to get elevation.")
     API_URL = 'https://maps.googleapis.com/maps/api/elevation/json' 
     params = {'key':KEY,'locations':'{lat},{lon}'.format(**coord)}
     r = requests.get(API_URL,params=params)
@@ -15,36 +15,45 @@ def getElevation(coord):
 
 
 def findGroundLocation(coord,alt,pitch,yaw):
-    dY = alt*np.tan(np.deg2rad(-pitch))
+    dY = alt*np.tan(np.deg2rad(pitch))
     location = llmath.atDistAndBearing(coord,dY,yaw)
     return location
 
 def getScanEdges(coord,alt,roll,pitch,yaw,view_angl=17):
     ground_pos = findGroundLocation(coord,alt,pitch,yaw)
-    thetaL = np.deg2rad(view_angl+roll)
-    thetaR = np.deg2rad(view_angl-roll)
-    dXl = alt*np.tan(np.deg2rad(thetaL))
-    dXr = alt*np.tan(np.deg2rad(thetaR))
-    lBound = llmath.atDistAndBearing(ground_pos,dXl,yaw+90)
-    rBound = llmath.atDistAndBearing(ground_pos,dXl,yaw-90)
+    thetaL = np.deg2rad(view_angl/2+roll)
+    thetaR = np.deg2rad(view_angl/2-roll)
+    dXl = alt*np.tan(thetaL)
+    dXr = alt*np.tan(thetaR)
+    lBound = llmath.atDistAndBearing(ground_pos,dXl,yaw-90)
+    rBound = llmath.atDistAndBearing(ground_pos,dXr,yaw+90)
     return lBound,rBound
 
 
-def processGPS(gps_file,out_shapefile='out',view_angl=17):
+def processGPS(gps_file,out_shapefile='out',view_angl=17,elevation =None):
     view_angl = float(view_angl)
     data = np.loadtxt(gps_file)
     lSide = []
     rSide = []
-    elevation = getElevation({'lat':data[0,1],'lon':data[0,0]})
-    for lon,lat,alt,roll,pitch,yaw in data[:,1:-1]:
+    if elevation is None:
+        elevation = getElevation({'lat':data[0,2],'lon':data[0,1]})
+    else:
+        elevation = float(elevation)
+    for i,(lon,lat,alt,roll,pitch,yaw) in enumerate(data[:,1:-1]):
         alt -= elevation
-        lBound,rBound = getScanEdges({'lat':lat,'lon':lon},alt,roll,pitch,yaw)
+        lBound,rBound = getScanEdges({'lat':lat,'lon':lon},alt,roll,pitch,yaw,view_angl)
         lSide.append(lBound)
         rSide.append(rBound)
 
     SHPParse.toPoly(lSide+rSide[::-1],elevation,out_shapefile)
 
 
+#pip install requests
+#pip install pyshp
+#python GPSParse.py input_gps.txt output.shp [optional: view_angle elevation]
 if __name__ =='__main__':
     import sys
+    #more command line:
+    #give elevation
+    #coarse view
     processGPS(*sys.argv[1:])
---
+++ README.txt
@@ -0,0 +1,8 @@
+Usage:
+python GPSParse.py input_gps.txt output.shp view_angle
+
+Install:
+pip install requests
+pip install pyshp
+
+Create a file key.txt and add a google api key to it
