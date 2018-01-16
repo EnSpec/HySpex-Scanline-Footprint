eea37ff5070d769f4ff708775a44a6c958f47317
Matthew Westphall <westphall@russell.wisc.edu>
2018-01-16 14:59:44 -0600

    added functionality to command line arguments

--- GPSParse.py
+++ GPSParse.py
@@ -4,6 +4,7 @@ import requests
 import shapefile
 import os
 import sys
+from matplotlib import pyplot as plt
 
 EARTH_RADIUS = 6.3710088e6 #meters
 
@@ -18,7 +19,7 @@ def toPoly(coords,elev,fname):
 
 def atDistAndBearing(start,dist,bearing):
     """Find the destination from coordinates start travelling 
-    `dist` meters at bearing `bearing`
+    dist meters at bearing bearing
     """
     lat0 = start['lat']
     lon0 = start['lon']
@@ -36,19 +37,21 @@ def atDistAndBearing(start,dist,bearing):
     lonf = ((np.rad2deg(lonrf)+540)%360) - 180
     return {'lat':latf,'lon':lonf}
 
-def getGoogleElevation(coord,keyfile="key.txt"):
+def getGoogleElevation(coords,keyfile="key.txt"):
     print("Using Google API to get elevation.")
     try:
         KEY = open(keyfile,'r').read().strip()
     except FileNotFoundError:
-        print("Google API key not found. Use the --key-file argument to"
-              " specify the location of your api key (default key.txt)")
+        print("Google API key not found at %s.\n"
+              "Use the --key-file argument to"
+              " specify the location of your API key (default key.txt)"%keyfile)
         exit(1)
 
     API_URL = 'https://maps.googleapis.com/maps/api/elevation/json' 
-    params = {'key':KEY,'locations':'{lat},{lon}'.format(**coord)}
+    locs = '|'.join(['{lat},{lon}'.format(**c)for c in coords])
+    params = {'key':KEY,'locations':locs}
     r = requests.get(API_URL,params=params)
-    return r.json()['results'][0]['elevation']
+    return [p['elevation'] for p in r.json()['results']]
 
 
 def findGroundLocation(coord,alt,pitch,yaw):
@@ -66,48 +69,82 @@ def getScanEdges(coord,alt,roll,pitch,yaw,view_angl=17):
     rBound = atDistAndBearing(ground_pos,dXr,yaw+90)
     return lBound,rBound
 
-def processGPS(gps_file,out_shapefile='out',view_angl=17,elevation =None):
-    view_angl = float(view_angl)
-    data = np.loadtxt(gps_file)
+LOG_HEADER="ImgName,CtrLon,CtrLat,GndDEMm,SensorMSLm,SensorAGLm,ImgFOV"
+def logMeta(log_fname,coord,gps_fname,ground_elev,alt_msl,fov):
+    im_fname = os.path.basename(gps_fname).replace('_raw.txt','')
+    alt_agl = alt_msl - ground_elev
+    print(LOG_HEADER)
+    meta_list = [im_fname,coord['lon'],coord['lat'],ground_elev,alt_msl,fov]
+    meta_str = ",".join(map(str,meta_list))
+    print(meta_str+"\n")
+    if os.path.exists(log_fname):
+        with open(log_fname,"a") as f:
+            f.write(meta_str+'\n')
+    else:
+        with open(log_fname,"w") as f:
+            f.write(LOG_HEADER+'\n')
+            f.write(meta_str+'\n')
+
+
+def processGPS(config):
+    view_angl = config.fov
+    data = np.loadtxt(config.gps_file)
     lSide = []
     rSide = []
+    elevation = config.elev
     if elevation is None:
-        elevation = getGoogleElevation({'lat':data[0,2],'lon':data[0,1]})
+        #get elevations from the google elevation api
+        ctrs = np.linspace(0,data.shape[0],config.npoints+2,dtype=int)[1:-1]
+        locs = [{'lat':data[i,2],'lon':data[i,1]}for i in ctrs]
+        midpt_elevs = getGoogleElevation(locs,config.keyfile)
+        elevation = np.interp(np.arange(data.shape[0]),ctrs,midpt_elevs)
     else:
-        elevation = float(elevation)
+        elevation = np.zeros(data.shape[0])+elevation
     for i,(lon,lat,alt,roll,pitch,yaw) in enumerate(data[:,1:-1]):
-        alt -= elevation
+        alt -= elevation[i]
+        if config.smooth:
+            roll = 0
         lBound,rBound = getScanEdges({'lat':lat,'lon':lon},alt,roll,pitch,yaw,view_angl)
         lSide.append(lBound)
         rSide.append(rBound)
+        if config.log_meta and i==int(data.shape[0]/2):
+            #log the midpoint of the scan as metadata
+            img_ctr = findGroundLocation({'lat':lat,'lon':lon},alt,pitch,yaw)
+            logMeta(config.log_meta,img_ctr,config.gps_file,np.mean(elevation),
+                    alt,view_angl)
 
-    toPoly(lSide+rSide[::-1],elevation,out_shapefile)
+    out = config.out or os.path.split(config.gps_file)[1].replace('.txt','')
+    toPoly(lSide+rSide[::-1],np.mean(elevation),out)
 
 
-#pip install requests
-#pip install pyshp
-#python GPSParse.py input_gps.txt output.shp [optional: view_angle elevation]
+
 IN_HELP="""
-input file of tab-separated gps coordinates. 
-Format: lon lat alt roll pitch yaw
+Input file of tab-separated gps coordinates. 
+File Format: lon lat alt roll pitch yaw
+"""
+OUT_HELP="""
+Name of shapefile to write (default <input_gps>.shp)
+"""
+FOV_HELP="""
+Field of view of spectrometer being used (default 17 degrees)
 """
 GOOGL_HELP="""
 Number of elevation points to request from the Google API (default 1). 
 Elevations are interpolated evenly throughout the scan area.
-Note: Requires a google API key.
+Note: Requires a google API key (see --key-file)
 """
 KEY_HELP="""
 Location of file containing Google API key to use for Google Maps elevation 
-requests. 
+requests. If not specified, program will try to read ./key.txt 
 """
 ELEV_HELP="""
 Use a single elevation value (in meters) for the whole reading instead of
-using the google elevation api.
+using the google elevation api
 """
-EFILE_HELP="""
-Read a list of elevations from a file instead of using the google elevation API.
-Elevations are interpolated to the closest given value.
-Format: lon lat elev
+SMOOTH_HELP="""Produce a shapefile with fewer jagged edges"""
+
+LOG_HELP="""
+Append metadata to a log file. Creates the log file if it doesn't exist
 """
 
 if __name__ =='__main__':
@@ -115,11 +152,15 @@ if __name__ =='__main__':
     parser = argparse.ArgumentParser(description=
             "Convert gps text file to shapefile")
     parser.add_argument("gps_file",help=IN_HELP)
-    parser.add_argument("--google-elev-points",help=GOOGL_HELP)    
-    parser.add_argument("--key-file",help=KEY_HELP)    
-    parser.add_argument("--elev",help=ELEV_HELP)
-    parser.add_argument("--elev-file",help=EFILE_HELP)
-    #more command line:
-    #give elevation
-    #coarse view
-    processGPS(*sys.argv[1:])
+    parser.add_argument("-o","--out",help=OUT_HELP)
+    parser.add_argument("-v","--fov",type=float,default=17,help=FOV_HELP)
+    parser.add_argument("-g","--google-elev",metavar="NPOINTS",dest="npoints",
+            type=int,default=1,help=GOOGL_HELP)    
+    parser.add_argument("-k","--key-file",metavar="KEY_FILE",dest="keyfile",
+            type=str,help=KEY_HELP,default="key.txt")
+    parser.add_argument("-e","--elev",type=int,help=ELEV_HELP)
+    parser.add_argument("-s","--smooth",help=SMOOTH_HELP,action='store_true')
+    parser.add_argument("-l","--log-meta",metavar="LOG_FILE",help=LOG_HELP)
+    args = parser.parse_args()
+    processGPS(args)
+
--- README.txt
+++ README.txt
@@ -1,8 +1,33 @@
-Usage:
-python GPSParse.py input_gps.txt output.shp [view_angle elevation]
-
 Install:
-pip install requests
-pip install pyshp
+pip install numpy requests pyshp
 
-Create a file key.txt and add a google api key to it
+Usage: GPSParse.py [-h] [-o OUT] [-v FOV] [-g NPOINTS] [-k KEY_FILE] [-e ELEV]
+                   [-s] [-l LOG_FILE]
+                   gps_file
+
+Convert gps log textfile to shapefile
+
+positional arguments:
+  gps_file              Input file of tab-separated gps coordinates. File
+                        Format: lon lat alt roll pitch yaw
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUT, --out OUT     Name of shapefile to write (default out.shp)
+  -v FOV, --fov FOV     Field of view of spectrometer being used (default 17
+                        degrees)
+  -g NPOINTS, --google-elev NPOINTS
+                        Number of elevation points to request from the Google
+                        API (default 1). Elevations are interpolated evenly
+                        throughout the scan area. Note: Requires a google API
+                        key (see --key-file)
+  -k KEY_FILE, --key-file KEY_FILE
+                        Location of file containing Google API key to use for
+                        Google Maps elevation requests. If not specified,
+                        program will try to read ./key.txt
+  -e ELEV, --elev ELEV  Use a single elevation value (in meters) for the whole
+                        reading instead of using the google elevation api
+  -s, --smooth          Produce a shapefile with fewer jagged edges
+  -l LOG_FILE, --log-meta LOG_FILE
+                        Append metadata to a log file. Creates the log file if
+                        it doesn't exist
